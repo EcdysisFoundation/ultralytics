24beba47babf07cc2815f6c55beec3df0c6d199a
Mikayla Wilson <mikaylaelectra@gmail.com>
2025-04-29 12:06:49 -0400

    update with current

--- train.py
+++ train.py
@@ -1,7 +1,4 @@
-from os import listdir
-from os.path import isfile, join, basename
-
 from ultralytics import YOLO
 
-model = YOLO('yolov8n.pt')  #'yolov8n.yaml' to use without pretrained weights or 'yolov8n.pt' for a pretrained model
-results = model.train(data='dataset.yaml', epochs=100, imgsz=640)
+model = YOLO('yolov8n.pt')
+results = model.train(data='datasets/data.yaml', epochs=100, imgsz=640)
